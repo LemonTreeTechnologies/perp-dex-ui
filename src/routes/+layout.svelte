bd8c38f8996bf066970e2f08361da71333218c77
8ball030 <8baller@station.codes>
2026-04-12 10:14:44 +0100

    chore:favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <div class="min-h-screen bg-[#0A0A0A]">
 	<Header />
 	<main class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
